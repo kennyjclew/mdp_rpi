2b3d0007f05dd4b32a4db1f389e2156621fd856d
Kenny Lew <kennyjclew@gmail.com>
2020-09-18 17:48:11 +0800

    testing camera

---
+++ Cameratest_2.py
@@ -0,0 +1,15 @@
+from picameraimport PiCamera
+from picamera.arrayimport PiRGBArray
+
+camera = PiCamera()
+camera.resolution= (640,480)
+output = PiRGBArray(camera)
+camera.capture(output,’bgr’)
+
+src = output.array
+print(‘Capture %dx%dimage’ %(src.shape[1],src.shape[0]))
+for i in range(1,10):
+	s=''
+	for j in range(1,10):
+		s=s+repr(src[i,j,1])+' '
+	print(s)
