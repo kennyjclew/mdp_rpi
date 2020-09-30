950073eeda209ef7b841303746a0b7bf328fab48
bryna <brynagoh@gmail.com>
2020-09-30 12:31:00 +0800

    Adds exception handling for end process

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from multithreading_temp import Multithreading 
 import time
-a = Multithreading()
-time.sleep(2)
-a.print_queue_now()
+
+try:
+    processes = Multithreading()
+except KeyboardInterrupt:
+    print("stopping all threads and disconnecting all devices")
+    processes.stop_all()
+    print("the end")
